7cd565b071e8a61b608ab848a9caf6f8207cd50f
isaac-gros <isaac.gros888@gmail.com>
2021-07-07 09:39:06 +0200

    Initial Commit

---
+++ SwiftMovies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftMovies
+//
+//  Created by jpo on 07/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftMovies/SwiftMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMoviesApp.swift
+//  SwiftMovies
+//
+//  Created by jpo on 07/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
